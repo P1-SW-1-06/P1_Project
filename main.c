a7947c94d79785b93b0397d89c4eabb6bcfe7e92
sebub <sebbemain@gmail.com>
2022-10-31 13:21:43 +0100

    j

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    struct person{
+        int Time;
+        int Env;
+        int Cost;
+        char* navn;
+    };
+
+    int personer;
+    printf("Enter the amount of people you want to optimize for!\n");
+    scanf("%d",&personer);
+
+    struct person Parray[personer];
+
+    int a = Parray[1].Env = 1;
+    int b = Parray[1].Time= 2;
+    int c = Parray[1].Cost = 1;
+    char* d = Parray[0].navn = "mogens";
+
+    printf("%d %d %d %s", a, b, c, d);
 }
