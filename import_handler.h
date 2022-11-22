4c7dd4ce9d9bb3e76b1460b7d678c83168fdea51
AndreasGriem <agriem22@student.aau.dk>
2022-11-22 10:57:11 +0100

    Flere kommentarer tilføjet

--- import_handler.h
+++ import_handler.h
@@ -12,7 +12,8 @@ typedef struct {
 } transport_struct;
 
 /**
- * @description A function that has a pointer to a file as input, opens the file transport_types.txt and checks if the file exists, allocate space in malloc for transport_data
+ * @description A function that has a pointer to a file as input, opens the file transport_types.txt and checks if the
+ * file exists, allocate space in malloc for transport_data
  * @return Returns a pointer to the input file
  */
 transport_struct *import_data();
@@ -25,7 +26,8 @@ transport_struct *import_data();
 int file_exists(FILE *file);
 
 /**
- * @description A function that reads a file and place the input in an array in a loop that runs until it no longer receives 4 input
+ * @description A function that reads a file and place the input in an array in a loop that runs until it no longer
+ * receives 4 input
  * @param data Is a pointer to an array of type transport_struct
  * @param file The file where the data is drawn from
  */
