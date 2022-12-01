8d9d05cc4ec1a5c1bf971c32ea9f16926562f2ca
EmilChri <113058804+EmilChri@users.noreply.github.com>
2022-12-01 14:37:07 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,18 +12,18 @@ int main() {
     user_input();
     transport_struct *transport_data = import_data();
 
-    int nr = find_number_of_cities();
-    char** city_name = create_city_name_array(nr);
+    int num_of_cities = find_number_of_cities();
+    char** city_name = create_city_name_array(num_of_cities);
 
-    char* PoW = place_of_work(city_name, nr);
+    char* PoW = place_of_work(city_name, num_of_cities);
 
     printf("%s",PoW);
 
-    int** city_distance_array = create_city_distance_matrix(nr, city_name);
+    int** city_distance_array = create_city_distance_matrix(num_of_cities, city_name);
 
-    int* dijkstra_array = dijkstra(city_distance_array, index_city_names(PoW, city_name, nr),nr);
+    int* dijkstra_array = dijkstra(city_distance_array, index_city_names(PoW, city_name, num_of_cities),num_of_cities);
     printf("The distance from your workplace to\n");
-    for (int i = 0; i < nr; ++i) {
+    for (int i = 0; i < num_of_cities; ++i) {
         if (dijkstra_array[i] <= 95) { // mangler MAX_distance i if statement i stedet for 95
             printf("%s is %d\n",city_name[i], dijkstra_array[i]);
         }
