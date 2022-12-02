3737b3a4763bb597e0a9ebafc6257335c66414f7
Peterrahr <Prni22@student.aau.dk>
2022-12-02 12:56:25 +0100

    update

--- src/score_algorithm.c
+++ src/score_algorithm.c
@@ -21,25 +21,12 @@ enviorment
 double* time_1(int* distance_array,int nr_city, people_data *final_user) {
 
     transport_struct *transport_data = import_data();
-
-
-
+    int j = 0;
     for (int i = 0; i < nr_city; ++i) {
-      double timescore = (transport_data[bus].speed / distance_array[i]) / final_user[0].max_time;
+      double timescore = (distance_array[i] / transport_data[bus].speed) / final_user[0].max_time
+              * final_user[0].preference_time;
       printf("%lf \n", timescore);
     }
 
 }
 
-
-    /*for (int i = 0; i < nr_city; ++i) {
-        printf("%d \n", distance_array[i]);
-    }
-
-}
-     */
-/*
- printf("%lf", transport_data[1].cost);
-
-((Shortest distance / speed) / max_time) * pref_time_point
- */
