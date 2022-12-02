200d70b912abf0f5dfd84d5d7e7679a5191714ce
sebub <sebbemain@gmail.com>
2022-12-02 12:11:23 +0100

    moved structs to main.h

--- src/main.h
+++ src/main.h
@@ -12,6 +12,36 @@
 // as #include guards, but with several advantages, including: less code, avoiding name clashes, and improved
 // compile speed.
 #pragma once
+typedef enum {
+    bus, car, bike
+} transport_types;
+
+typedef enum {
+    frederikshavn, aalborg, hjoerring, hobro, stoevring, thisted, noerresundby, broenderslev
+} cities;
+
+typedef struct {
+    int include_car;
+    int include_bus;
+    int include_bike;
+    int include_train;
+} exclusions;
+
+typedef struct {
+    char name[50];
+    int max_time;
+    int preference_environment;
+    int preference_cost;
+    int preference_time;
+    transport_types included_transport_types[4];
+    exclusions exclusion;
+    char *place_of_work;
+} people_data;
+
+typedef struct {
+    char name[50];
+
+} city_data;
 
 /* -- Includes -- */
 #include <stdio.h>
--- src/score_algorithm.h
+++ src/score_algorithm.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "main.h"
 #include "user_input.h"
 
 double* time_1(int* distance_array,int nr_city, people_data * final_user);
