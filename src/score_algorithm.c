17042ab72f3ea8482368075f3b27325b45d11f21
Peterrahr <Prni22@student.aau.dk>
2022-12-02 13:07:54 +0100

    trans inc

--- src/score_algorithm.c
+++ src/score_algorithm.c
@@ -22,11 +22,39 @@ double* time_1(int* distance_array,int nr_city, people_data* final_user) {
 
     transport_struct *transport_data = import_data();
 
-    for (int i = 0; i < nr_city; ++i) {
-      double timescore = (distance_array[i] / transport_data[bus].speed) / final_user[0].max_time
-              * final_user[0].preference_time;
-      printf("%lf \n", timescore);
+    int j = 0;
+
+    if (final_user[j].exclusion.include_bus == 1) {
+        for (int i = 0; i < nr_city; ++i) {
+            double timescore = (distance_array[i] / transport_data[bus].speed) / final_user[0].max_time
+                               * final_user[0].preference_time;
+            printf("timescore bus: %lf \n", timescore);
+        }
+    }
+    else {
+
     }
 
+    if (final_user[j].exclusion.include_car == 1) {
+        for (int i = 0; i < nr_city; ++i) {
+            double timescore = (distance_array[i] / transport_data[car].speed) / final_user[0].max_time
+                               * final_user[0].preference_time;
+            printf("timescore car: %lf \n", timescore);
+        }
+    }
+    else {
+
+    }
+
+    if (final_user[j].exclusion.include_bike == 1) {
+        for (int i = 0; i < nr_city; ++i) {
+            double timescore = (distance_array[i] / transport_data[bike].speed) / final_user[0].max_time
+                               * final_user[0].preference_time;
+            printf("timescore bike: %lf \n", timescore);
+        }
+    }
+    else {
+
+    }
 }
 
