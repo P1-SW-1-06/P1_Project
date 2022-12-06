b38c71dd9ce6e1fc04389f510b6c02be566de473
sebub <sebbemain@gmail.com>
2022-12-06 15:16:46 +0100

    changed to voids and fixed malloc problem

--- src/score_algorithm.c
+++ src/score_algorithm.c
@@ -2,144 +2,46 @@
 #include "structs.h"
 
 
-temp_score* time_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data);
 
-double score_algorithm(people_data* final_user, int nr_city, int number_of_people){
-
-    /*double people_score_time[100] = {};
-
-    //time_1(dijkstra_array,nr, final_user_input, nr);
-
-    //people_score_time[bus] = timescore_bus; */
-
-
-
-
-}
-
-temp_score* time_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data) {
-
-
-    int user = 0;
-    temp_score *people_score_time = malloc(number_of_people * sizeof(temp_score));
-    if (people_score_time == NULL) {
+temp_score* score_algorithm(int* distance, people_data* final_user, int nr_city, int number_of_people,transport_struct* transport_data){
+    temp_score *score_array = malloc(number_of_people * sizeof(temp_score));
+    if (score_array == NULL) {
         printf("Memmory not allocated");
     }
 
 
-    for (int city = 0; city < nr_city; ++city) {
-        if (final_user[user].exclusion.include_bus == 1) {
-            people_score_time[city].time.score_bus = (distance[city] / transport_data[bus].speed) / final_user[user].max_time
-                                               * final_user[user].preference_time;
-
-            printf("timescore bus: %lf \n", people_score_time[city].time.score_bus);
-        }
-
-
-            if (final_user[user].exclusion.include_car == 1) {
-                    people_score_time[city].time.score_car =
-                            (distance[city] / transport_data[car].speed) / final_user[user].max_time
-                                                * final_user[user].preference_time;
-
-                    printf("timescore car: %lf \n", people_score_time[city].time.score_car);
-            }
-
-            if (final_user[user].exclusion.include_bike == 1) {
-                    people_score_time[city].time.score_bike =
-                            (distance[city] / transport_data[bike].speed) / final_user[user].max_time
-                                                * final_user[user].preference_time;
-
-                    printf("timescore bike: %lf \n", people_score_time[city].time.score_bike);
-            }
-        }
-
-    return people_score_time;
+    for (int i = 0; i < nr_city; ++i) {
+        time_score(score_array, distance,final_user,transport_data,i);
+        env_score(score_array, distance, final_user, transport_data, i);
+        transport_cost_score(score_array, distance, final_user, transport_data, i);
     }
-/*
-double max_co2(people_data* final_user, transport_struct* transport_data){
-    int user = 0;
-    double result_co2 = (final_user[user].max_time / (1 / transport_data[car].speed) * transport_data[car].co2 * 189 * 2);
-    return result_co2;
-}
-*/
+    return score_array;
 
-temp_score* env_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data) {
-
-
-    temp_score *people_score_environment = malloc(number_of_people * sizeof(temp_score));
-    if (people_score_environment == NULL) {
-        printf("Memmory not allocated");
-    }
-
-    int user = 0;
-
-    double max_result_co2 = (final_user[user].max_time / (1 / transport_data[car].speed)
-                                                    * transport_data[car].co2 * 189 *2);
-
-
-    for (int city = 0; city < nr_city; ++city) {
-        if (final_user[user].exclusion.include_bus == 1) {
-            people_score_environment[city].enviroment.score_bus = ((distance[city] * transport_data[bus].co2 * 189 * 2)
-                                                                   / max_result_co2 *
-                                                                   final_user[user].preference_environment);
-
-            printf("enviroment score bus: %lf \n", people_score_environment[city].enviroment.score_bus);
-        }
-        if (final_user[user].exclusion.include_car == 1) {
-            people_score_environment[city].enviroment.score_car = ((distance[city] * transport_data[car].co2 * 189 * 2)
-                                                                   / max_result_co2 *
-                                                                   final_user[user].preference_environment);
-
-            printf("enviroment score car: %lf \n", people_score_environment[city].enviroment.score_car);
-        }
-        if (final_user[user].exclusion.include_bike == 1) {
-            people_score_environment[city].enviroment.score_bike = (
-                    (distance[city] * transport_data[bike].co2 * 189 * 2)
-                    / max_result_co2 * final_user[user].preference_environment);
-
-            printf("enviroment score bike: %lf \n", people_score_environment[city].enviroment.score_bike);
-            printf("HEj: %lf\n", people_score_environment[2].enviroment.score_bus);
-        }
-    }
-    return people_score_environment;
 }
 
-temp_score* transport_cost_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data) {
-
-    temp_score *people_score_transport_cost = malloc(number_of_people * sizeof(temp_score));
-    if (people_score_transport_cost == NULL) {
-        printf("Memmory not allocated");
+void time_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city) {
+    scorearray[city].time.score_bus = (distance[city]/transport_data[bus].speed) / final_user[0].max_time * final_user[0].preference_time;
+    scorearray[city].time.score_car = (distance[city]/transport_data[car].speed) / final_user[0].max_time * final_user[0].preference_time;
+    scorearray[city].time.score_bike = (distance[city]/transport_data[bike].speed) / final_user[0].max_time * final_user[0].preference_time;
     }
 
+void env_score(temp_score *scorearray ,int* distance, people_data* final_user, transport_struct* transport_data, int city) {
 
-    int user = 0;
+    double max_result_co2 = (final_user[0].max_time / (1 / transport_data[car].speed) * transport_data[car].co2 * 189 *2);
+    scorearray[city].enviroment.score_bus = (distance[city]*transport_data[bus].co2*189*2) / max_result_co2 * final_user[0].preference_environment;
+    scorearray[city].enviroment.score_car = (distance[city]*transport_data[car].co2*189*2) / max_result_co2 * final_user[0].preference_environment;
+    scorearray[city].enviroment.score_bike = (distance[city]*transport_data[bike].co2*189*2) / max_result_co2 * final_user[0].preference_environment;
 
-    double max_result_transport_cost = (final_user[user].max_time / (1 / transport_data[car].speed)
+
+}
+
+void transport_cost_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city) {
+
+    double max_result_transport_cost = (final_user[0].max_time / (1 / transport_data[car].speed)
                                         * transport_data[car].cost * 189 * 2);
+    scorearray[city].transport_cost.score_bus = ((distance[city] * transport_data[bus].cost * 189 * 2) / max_result_transport_cost * final_user[0].preference_cost / 2);
+    scorearray[city].transport_cost.score_car = ((distance[city] * transport_data[car].cost * 189 * 2) / max_result_transport_cost * final_user[0].preference_cost / 2);
+    scorearray[city].transport_cost.score_bike = ((distance[city] * transport_data[bike].cost * 189 * 2) / max_result_transport_cost * final_user[0].preference_cost / 2);
 
-    for (int city = 0; city < nr_city; ++city) {
-        if (final_user[user].exclusion.include_bus == 1) {
-            people_score_transport_cost[city].transport_cost.score_bus = ((distance[city]
-                                                                           * transport_data[bus].cost * 189 * 2) /
-                                                                          max_result_transport_cost *
-                                                                          final_user[user].preference_cost / 2);
-            printf("transport cost score bus: %lf \n", people_score_transport_cost[city].transport_cost.score_bus);
-        }
-        if (final_user[user].exclusion.include_car == 1) {
-            people_score_transport_cost[city].transport_cost.score_car = ((distance[city]
-                                                                           * transport_data[car].cost * 189 * 2) /
-                                                                          max_result_transport_cost *
-                                                                          final_user[user].preference_cost / 2);
-            printf("transport cost score car: %lf \n", people_score_transport_cost[city].transport_cost.score_car);
-        }
-        if (final_user[user].exclusion.include_bike == 1) {
-            people_score_transport_cost[city].transport_cost.score_bike = ((distance[city]
-                                                                            * transport_data[bike].cost * 189 * 2) /
-                                                                           max_result_transport_cost *
-                                                                           final_user[user].preference_cost / 2);
-            printf("transport cost score bike: %lf \n", people_score_transport_cost[city].transport_cost.score_bike);
-        }
-    }
-    return people_score_transport_cost;
 }
 
--- src/score_algorithm.h
+++ src/score_algorithm.h
@@ -3,8 +3,8 @@
 #include "user_input.h"
 #include "import_handler.h"
 
-
-temp_score* time_score(int* distance ,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data);
+temp_score* score_algorithm(int* distance, people_data* final_user, int nr_city, int number_of_people,transport_struct* transport_data);
+void time_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city);
 double max_co2(people_data* final_user, transport_struct* transport_data);
-temp_score* env_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data);
-temp_score* transport_cost_score(int* distance,int nr_city, people_data* final_user, int number_of_people, transport_struct* transport_data);
+void env_score(temp_score *scorearray ,int* distance, people_data* final_user, transport_struct* transport_data, int city);
+void transport_cost_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city);
