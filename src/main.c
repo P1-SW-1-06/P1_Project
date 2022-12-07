15a5b2de2500d60b15b48b7d92ccd21be807f31a
AndreasGriem <agriem22@student.aau.dk>
2022-12-07 15:54:09 +0100

    ting og sager

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ int main() {
 
     transport_struct *transport_data = import_vehicle_data("transport_types.txt");
     int num_of_cities = find_number_of_cities("city_information.txt");
-    char** city_name = import_city_names("city_information.txt" ,num_of_cities);
-    int** city_distance_array = import_city_distances("cities.txt",
+    char **city_name = import_city_names("city_information.txt", num_of_cities);
+    int **city_distance_array = import_city_distances("cities.txt",
                                                       num_of_cities,
                                                       city_name);
     /*for (int j = 0; j < num_of_cities; ++j) {
@@ -25,10 +25,11 @@ int main() {
 
 
     int number_of_people = scan_number_of_people();
-    people_data* user_data = collect_user_input(city_name,num_of_cities, number_of_people);
+    people_data *user_data = collect_user_input(city_name, num_of_cities, number_of_people);
 
 
-    temp_score* score = score_algorithm(city_distance_array,user_data,num_of_cities,number_of_people,transport_data);
+    temp_score **score = score_algorithm(city_distance_array, user_data, num_of_cities, number_of_people,
+                                        transport_data);
 
 /*
     for (int i = 0; i < num_of_cities; ++i) {
@@ -54,13 +55,27 @@ int main() {
 
     }
 */
-    for (int i = 0; i < num_of_cities; ++i) {
-        double bus_score = score[i].time.score_bus + score[i].enviroment.score_bus + score[i].final_cost.score_bus;
-        double car_score = score[i].time.score_car + score[i].enviroment.score_car + score[i].final_cost.score_car;
-        double bike_score = score[i].time.score_bike + score[i].enviroment.score_bike + score[i].final_cost.score_bike;
+    for (int j = 0; j < number_of_people; ++j) {
+        for (int i = 0; i < num_of_cities; ++i) {
+
+            double car_score;
+            double bus_score;
+            double bike_score;
+
+            if (user_data[j].exclusion.include_bus == 0) { bus_score = -1; }
+            else bus_score = score[j][i].time.score_bus + score[j][i].enviroment.score_bus + score[j][i].final_cost.score_bus;
+
+            if (user_data[j].exclusion.include_car == 0) { car_score = -1; }
+            else car_score = score[j][i].time.score_car + score[j][i].enviroment.score_car + score[j][i].final_cost.score_car;
+
+            if (user_data[j].exclusion.include_bike == 0) { bike_score = -1; }
+            else
+                bike_score = score[j][i].time.score_bike + score[j][i].enviroment.score_bike + score[j][i].final_cost.score_bike;
 
 
-        printf("\n City: %s\n BUSSCORE: %.2lf\n CARSCORE: %.2lf\n BIKESCORE: %.2lf\n ",city_name[i],bus_score, car_score, bike_score);
+            printf("\n City: %s\n BUSSCORE: %.2lf\n CARSCORE: %.2lf\n BIKESCORE: %.2lf\n ", city_name[i], bus_score,
+                   car_score, bike_score);
 
+        }
     }
 }
--- src/score_algorithm.h
+++ src/score_algorithm.h
@@ -3,12 +3,12 @@
 #include "user_input.h"
 #include "import_handler.h"
 
-temp_score* score_algorithm(int** distance_array, people_data* final_user, int nr_city, int number_of_people,transport_struct* transport_data);
-void time_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city);
+temp_score **score_algorithm(int** distance_array, people_data* final_user, int nr_city, int number_of_people,transport_struct* transport_data);
+void time_score(temp_score **scorearray,int person_number, int *distance, people_data *final_user, transport_struct *transport_data, int city);
 double max_co2(people_data* final_user, transport_struct* transport_data);
-void env_score(temp_score *scorearray ,int* distance, people_data* final_user, transport_struct* transport_data, int city);
-void transport_cost_score(temp_score *scorearray, int* distance, people_data* final_user, transport_struct* transport_data, int city);
-void housing_cost_score(temp_score* scorearray, people_data *final_user, double yearly_max_housing_cost, int current_city, int city);
+void env_score(temp_score **scorearray,int person_number, int *distance, people_data *final_user, transport_struct *transport_data, int city);
+void transport_cost_score(temp_score **scorearray,int person_number, int *distance, people_data *final_user, transport_struct *transport_data,int city);
+void housing_cost_score(temp_score **scorearray, int person_number, double yearly_max_housing_cost, int current_city, int city);
 double max_housing_price(int* array_housing_cost,int nr_city);
-double max_result_transport_cost(transport_struct *transport_data, people_data *final_user);
-void sum_of_cost_scores(temp_score *scorearray, people_data *final_user, int city, double max_result_transport_cost,double yearly_max_housing_cost);
+double max_result_transport_cost(int person_number, transport_struct *transport_data, people_data *final_user);
+void sum_of_cost_scores(int person_number,temp_score **scorearray, people_data *final_user, int city, double max_result_transport_cost,double yearly_max_housing_cost);
