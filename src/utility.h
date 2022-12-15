be70e49e09c997eb1d784110a8fc3759b983b22f
od45pv <od45PV@student.aau.dk>
2022-12-15 13:30:00 +0100

    Clean up

--- src/utility.h
+++ src/utility.h
@@ -29,11 +29,11 @@ bool confirm_choice();
 /**
  * Prints the final sorted score of cities to the user. Frees memory allocated with malloc in functions
  * collect_user_input(), score_algorithm() and final_output. *
- * @param number_of_people
- * @param number_of_available_cities
- * @param final_combined_city_score
- * @param score
- * @param user_data
+ * @param number_of_people Number of people to display
+ * @param number_of_available_cities Number of cities to display
+ * @param final_combined_city_score Final city score
+ * @param score Vehicle winner
+ * @param user_data Uses for name of person
  */
 void print_final_score(int number_of_people, int number_of_available_cities, shared_score *final_combined_city_score,
                        temp_score **score, people_data *user_data);
@@ -42,4 +42,10 @@ void print_final_score(int number_of_people, int number_of_available_cities, sha
  * Prints a guide of how to use the program
  */
 void print_user_guide();
+
+/**
+ * Handles selection of dataset for the specific city
+ * @param distance .txt file with distances to nearby cities
+ * @param housing .txt file with housing costs
+ */
 void choose_dataset(char* distance, char* housing);
